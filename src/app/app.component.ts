c6353c0a21484e70dad8d3bb432f64c193f6f7ed
Filip Komogorov <filipkomogorov@gmail.com>
2023-05-07 22:07:40 +0200

    adding styles to navbar color on scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  navbg: any;
+  @HostListener('document:scroll') scrollover() {
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+      this.navbg = {
+        'background-color': '#000',
+      };
+    } else {
+      this.navbg = {};
+    }
+  }
+}
